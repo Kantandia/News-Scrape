c7d734ebf4b76b50d6901cd4566b41012066dd43
Kantandia <taylorsdsmith@gmail.com>
2019-07-25 11:48:24 -0500

    added routes for data

--- routes/index.js
+++ routes/index.js
@@ -15,3 +15,23 @@ router.get('/', (req,res) =>  {
 router.get('/scraper', (req,res) =>  {
     request('https://cleaningtheglass.com/articles/', (error, response, html) =>  { 
     const $ = cheerio.load(html);
+
+
+    $('a.card.article').each((i,elem) =>  {
+        var article = {}
+        var x = $(elem)
+        article.title = x.children('h2').contents().filter(function()  {
+          return this.nodeType == 3
+        })[0].nodeValue;
+        article.summary = x.find('.article_description').text().trim();
+        article.link = x.attr('href').trim()
+        article.image = x.find('img').attr('src').trim()
+  
+        console.log(article);
+        Article.create(article)
+          .then((data) => console.log(data))
+          .catch((err) => res.json(err))
+      })
+      res.redirect('/articles')
+    })
+  })
